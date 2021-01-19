e7e7128f16180777fd8ea3158bfcbe1ac2be02b8
Kaneria <kaneria.dilipbhai@thomsonreuters.com>
2021-01-19 11:04:49 +0530

    add http to product info app

--- TwoWayBinding (Product Info Table)/info-table/src/app/components/product-info-table/product-info-table.component.ts
+++ TwoWayBinding (Product Info Table)/info-table/src/app/components/product-info-table/product-info-table.component.ts
@@ -2,22 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { GetProductInfoService } from '../../services/get-product-info.service';
 import { ProductInfo } from '../../models/ProductInfo';
 
-
 @Component({
   selector: 'app-info-table',
   templateUrl: './product-info-table.component.html',
-  styleUrls: ['./product-info-table.component.css']
+  styleUrls: ['./product-info-table.component.css'],
 })
 export class ProductInfoTableComponent implements OnInit {
   productInfo: ProductInfo[];
-  searchText = "";
-  constructor(private getProductInfoService: GetProductInfoService) { }
+  searchText = '';
+  addProductForm=false
+  constructor(private getProductInfoService: GetProductInfoService) {}
 
   ngOnInit(): void {
-    this.productInfo = this.getProductInfoService.getProductInfo()
+    this.getProductInfoService
+      .getProductInfo()
+      .subscribe((products) => (this.productInfo = products));
+  }
+  createProduct(product:ProductInfo) {
+    this.getProductInfoService.createProduct(product);
+    // console.log(product)
+  }
+  updateProduct(product:ProductInfo): void {
+    this.getProductInfoService.updateProduct(product);
+  }
+  deleteProduct(product:ProductInfo): void {
+    this.getProductInfoService.deleteProduct(product);
   }
   displayInfo(ele: ProductInfo) {
-    alert(`Name: ${ele.name} \nCode: ${ele.code} \nBranch Name: ${ele.branchName} \nBranch Code: ${ele.branchCode}\nAddress: ${ele.address}`)
+    alert(
+      `Name: ${ele.name} \nCode: ${ele.code} \nBranch Name: ${ele.branchName} \nBranch Code: ${ele.branchCode}\nAddress: ${ele.address}`
+    );
   }
-
 }
--- TwoWayBinding (Product Info Table)/info-table/src/app/services/get-product-info.service.ts
+++ TwoWayBinding (Product Info Table)/info-table/src/app/services/get-product-info.service.ts
@@ -1,33 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { Injectable } from '@angular/core';
+
+import { ProductInfo } from '../models/ProductInfo';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GetProductInfoService {
-  constructor() {}
+  constructor(private http: HttpClient) { }
   getProductInfo() {
-    return [
-      {
-        name: 'Product 1',
-        code: '3453',
-        branchName: 'branch-5',
-        branchCode: '7464',
-        address: 'vadodara',
-      },
-      {
-        name: 'Product 2',
-        code: '646',
-        branchName: 'branch-7',
-        branchCode: '567',
-        address: 'vadodara',
-      },
-      {
-        name: 'Product 3',
-        code: '3453',
-        branchName: 'branch-2',
-        branchCode: '7567',
-        address: 'vadodara',
-      },
-    ];
+    return this.http.get<ProductInfo[]>(
+      'http://localhost:4200/assets/products.json'
+    );
+  }
+  createProduct(product: ProductInfo) {
+    // Using Json Placeholder to get proper response from server
+    this.http.post(
+      'https://jsonplaceholder.typicode.com/posts', JSON.stringify(product)
+    ).subscribe(res => console.log(res))
+    console.log("Created", JSON.stringify(product))
+  }
+  updateProduct(product: ProductInfo) {
+    // Using Json Placeholder to get proper response from server
+    this.http.put(
+      'https://jsonplaceholder.typicode.com/posts/1', JSON.stringify(product)
+    ).subscribe(res => console.log(res))
+    console.log("Updated", JSON.stringify(product))
+  }
+  deleteProduct(product: ProductInfo) {
+    // Using Json Placeholder to get proper response from server
+    this.http.delete(
+      'https://jsonplaceholder.typicode.com/posts/1'
+    ).subscribe(res => console.log(res))
+    console.log("Deleted", JSON.stringify(product))
   }
 }
